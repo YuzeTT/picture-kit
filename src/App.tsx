860b57f5512734b530d97bdcf6768c956f544cef
YuzeTT <ziv1024@163.com>
2023-10-18 15:58:44 +0800

    chore: add notice

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,14 @@ function App() {
         <Tabs defaultActiveKey="1" items={items} tabBarExtraContent={{left: <div className='w-4' />}} />
         <div className='px-4 py-2 max-w-lg mx-auto'>
           <Alert message="本站仅供娱乐，所造成的一切后果与本站无关" className='mb-2' type="warning" showIcon closable/>
+          <Alert
+            message="公告"
+            description={<>
+              <div>本站永久免费，如需要其他模板请联系vx: hong_yu1024</div>
+              <div className='font-bold'>如果您有github帐号欢迎给我个Star，万分感激！！！ <a href="https://github.com/YuzeTT/picture-kit">转到Github</a></div>
+            </>}
+            type="success"
+          />
           <LuckinCoffee></LuckinCoffee>
         </div>
         <div className='text-xs text-center py-3 op-50'>© 2023 YuzeTT</div>
